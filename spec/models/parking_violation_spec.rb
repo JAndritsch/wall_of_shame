57fe9c9a67a155d9096c40d20a37c4694729b01e
jcarley <jcarley@gettyimages.com>
2013-04-17 16:48:23 -0500

    Cleans up parking violation tests.

--- spec/models/parking_violation_spec.rb
+++ spec/models/parking_violation_spec.rb
@@ -1,33 +1,28 @@
 require 'spec_helper'
 
 describe ParkingViolation do
-  let(:violation) { ParkingViolation.new }
+
+  subject { FactoryGirl.build(:parking_violation) }
 
   describe "attributes" do
-    it { violation.respond_to?(:created_at).should be_true }
-    it { violation.respond_to?(:description).should be_true }
-    it { violation.respond_to?(:car).should be_true }
+    it { should respond_to(:created_at) }
+    it { should respond_to(:description) }
+    it { should respond_to(:car) }
   end
 
-  describe "initialization" do
-    let(:car) { Car.new }
-    it "accepts a car" do
-      violation = ParkingViolation.new(:car => car)
-      violation.car.should == car
-    end
+  describe "associations" do
+    it { should belong_to(:car) }
+    it { should allow_mass_assignment_of(:car) }
   end
 
   describe "validations" do
     let(:car) { Car.new(:getty => true) }
-    it "cannot be assigned to a Getty vehicle" do
-      violation.car = car
-      violation.valid?.should be_false
-    end
 
-    it "requires a car" do
-      violation.valid?.should be_false
-      violation.car = Car.create(:plate_number => "1", :plate_state => "WI")
-      violation.valid?.should be_true
+    it { should validate_presence_of(:car) }
+
+    it "cannot be assigned to a Getty vehicle" do
+      subject.car = car
+      expect(subject).to_not be_valid
     end
   end
 
