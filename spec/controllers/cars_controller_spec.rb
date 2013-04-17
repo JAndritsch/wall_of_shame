12f62272e96cb375e38c6ba4f57b7ba24e1756b6
jcarley <jcarley@gettyimages.com>
2013-04-17 16:52:23 -0500

    Whitespace cleanup.

--- spec/controllers/cars_controller_spec.rb
+++ spec/controllers/cars_controller_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-describe CarsController do 
+describe CarsController do
   describe "edit" do
     it "looks up the car and renders you a form for editing" do
       Car.should_receive(:find).and_return(Car.new)
